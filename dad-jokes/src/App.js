a98ecf198da58ee7e6e6150de3dfdb1d82f6301f
Heorhii Siburov <g.siburov@gmail.com>
2019-10-18 10:55:01 -0700

    privateRoute working now

--- dad-jokes/src/App.js
+++ dad-jokes/src/App.js
@@ -3,7 +3,9 @@ import './App.css';
 import 'semantic-ui-css/semantic.min.css'
 import { BrowserRouter as Router, Route } from "react-router-dom";
 
+import PrivateRoute from './components/PrivateRoute';
 import LoginForm from './components/LoginForm';
+import Dashboard from './components/dashboard';
 
 function App() {
   return (
@@ -13,6 +15,7 @@ function App() {
           <h1>Sprint Dad Jokes App</h1>
         </header>
         <Route exact path="/" component={LoginForm} />
+        <PrivateRoute exact path="/dashboard" component={Dashboard} />
       </div>
     </Router>
   );
---
+++ dad-jokes/src/components/dashboard.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+
+function Dashboard(props) {
+
+
+  return (
+    <div className="DashBlock">
+        <h2>Dad Jokes</h2>
+    </div>
+  );
+}
+
+export default Dashboard;
