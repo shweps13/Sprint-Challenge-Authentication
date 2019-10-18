eccd55a818b5db7976053d6a6ef9e2b1a3c8ea21
Heorhii Siburov <g.siburov@gmail.com>
2019-10-18 08:23:49 -0700

    login/register with token is working

--- auth/auth-router.js
+++ auth/auth-router.js
@@ -2,15 +2,56 @@ const router = require('express').Router();
 const bcrypt = require('bcryptjs');
 const jwt = require('jsonwebtoken');
 
-const auth = require('./auth-model.js');
+const Auth = require('./auth-model.js');
 const secrets = require('../config/secretConfig.js')
 
 router.post('/register', (req, res) => {
-  // implement registration
+  let user = req.body;
+  const hash = bcrypt.hashSync(user.password, 10); 
+  user.password = hash;
+
+  Auth.add(user)
+    .then(saved => {
+      res.status(201).json(saved);
+    })
+    .catch(error => {
+      res.status(500).json({ message: 'cannot add the user', error} );
+    });
 });
 
 router.post('/login', (req, res) => {
-  // implement login
+  let { username, password } = req.body;
+
+  Auth.findBy({ username })
+    .first()
+    .then(user => {
+      if (user && bcrypt.compareSync(password, user.password)) {
+        const token = generateToken(user);
+        res.status(200).json({
+          message: `Welcome ${user.username}!`,
+          token,
+        });
+      } else {
+        res.status(401).json({ message: 'You shall not pass!' });
+      }
+    })
+    .catch(error => {
+      res.status(500).json(error);
+    });
 });
+
+function generateToken(user) {
+  const payload = {
+    username: user.username,
+    subject: user.id
+  };
+  
+  const options = {
+    expiresIn: '2h',
+  }
+
+  return jwt.sign(payload, secrets.jwtSecret, options);
+}
+
 
 module.exports = router;
