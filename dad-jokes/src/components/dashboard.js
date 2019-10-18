caa917d02287686a0a21fe84cea4ccd80955e43f
Heorhii Siburov <g.siburov@gmail.com>
2019-10-18 10:59:14 -0700

    fetching data from api with token

--- dad-jokes/src/components/dashboard.js
+++ dad-jokes/src/components/dashboard.js
@@ -1,8 +1,21 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import axiosWithAuth from "../utilites/axiosWithAuth";
 
 
-function Dashboard(props) {
+function Dashboard() {
 
+const [jokes, setJokes] = useState([]);
+
+console.log(jokes)
+
+useEffect(() => {
+    axiosWithAuth()
+    .get('/jokes')
+    .then(res => {
+        setJokes(res.data);
+    })
+    .catch(err => console.log(err));
+  }, []);
 
   return (
     <div className="DashBlock">
