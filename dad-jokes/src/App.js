d9568d7f1b7c7c2d9650058401ddc7436140aca4
Heorhii Siburov <g.siburov@gmail.com>
2019-10-18 11:27:21 -0700

    menu was added

--- dad-jokes/src/App.js
+++ dad-jokes/src/App.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import './App.css';
 import 'semantic-ui-css/semantic.min.css'
-import { BrowserRouter as Router, Route } from "react-router-dom";
+import { BrowserRouter as Router, Route, Link } from "react-router-dom";
 
 import PrivateRoute from './components/PrivateRoute';
 import LoginForm from './components/LoginForm';
@@ -13,6 +13,11 @@ function App() {
       <div className="App">
         <header className="App-header">
           <h1>Sprint Dad Jokes App</h1>
+          <div className="Menu">
+            <Link to="/">Login</Link>
+            <Link to="/register">Register</Link>
+            <Link to="/dashboard">Jokes</Link>
+          </div>
         </header>
         <Route exact path="/" component={LoginForm} />
         <PrivateRoute exact path="/dashboard" component={Dashboard} />
