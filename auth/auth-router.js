a6ff6bdc2efba8157493864647abd01f5ebc4930
Heorhii Siburov <g.siburov@gmail.com>
2019-10-18 08:43:20 -0700

    hotfix

--- auth/auth-router.js
+++ auth/auth-router.js
@@ -8,7 +8,7 @@ const secrets = require('../config/secretConfig.js')
 const validateAccountData = (req,res,next) => {
   const {body} = req;
 
-  if(!body.name || !body.password){
+  if(!body.username || !body.password){
       res.status(400).json({message: "Please provide a name and password"})
   }
   else {
