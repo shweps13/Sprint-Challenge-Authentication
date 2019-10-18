2a407d0d16d3d628f99e76124bdd13f9c7611404
Heorhii Siburov <g.siburov@gmail.com>
2019-10-18 11:41:13 -0700

    logout was added, register was builded

--- dad-jokes/src/App.js
+++ dad-jokes/src/App.js
@@ -2,12 +2,19 @@ import React from 'react';
 import './App.css';
 import 'semantic-ui-css/semantic.min.css'
 import { BrowserRouter as Router, Route, Link } from "react-router-dom";
+import { Button } from 'semantic-ui-react'
 
 import PrivateRoute from './components/PrivateRoute';
 import LoginForm from './components/LoginForm';
 import Dashboard from './components/dashboard';
+import Signup from './components/Signup';
 
 function App() {
+
+  const logout = () => {
+    window.localStorage.removeItem('token')
+  }
+
   return (
     <Router>
       <div className="App">
@@ -15,11 +22,13 @@ function App() {
           <h1>Sprint Dad Jokes App</h1>
           <div className="Menu">
             <Link to="/">Login</Link>
-            <Link to="/register">Register</Link>
+            <Link to="/signup">Register</Link>
             <Link to="/dashboard">Jokes</Link>
+            <Link onClick={logout} to="/">Log Out</Link>
           </div>
         </header>
         <Route exact path="/" component={LoginForm} />
+        <Route exact path="/signup" component={Signup} />
         <PrivateRoute exact path="/dashboard" component={Dashboard} />
       </div>
     </Router>
