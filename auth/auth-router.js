3d15344334eff58430a108db86eb434ae1c53e66
Heorhii Siburov <g.siburov@gmail.com>
2019-10-18 08:17:50 -0700

    model created, working on auth router

--- auth/auth-router.js
+++ auth/auth-router.js
@@ -1,4 +1,9 @@
 const router = require('express').Router();
+const bcrypt = require('bcryptjs');
+const jwt = require('jsonwebtoken');
+
+const auth = require('./auth-model.js');
+const secrets = require('../config/secretConfig.js')
 
 router.post('/register', (req, res) => {
   // implement registration
