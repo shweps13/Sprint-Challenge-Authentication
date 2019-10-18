93b9a6e131a72c1e2b8b4a92de1fd15b0dc53967
Heorhii Siburov <g.siburov@gmail.com>
2019-10-18 10:31:36 -0700

    react app was reinstalled

--- dad-jokes/src/App.js
+++ dad-jokes/src/App.js
@@ -1,19 +1,13 @@
 import React from 'react';
 import './App.css';
-import { BrowserRouter as Router, Route } from "react-router-dom";
-
-import LoginForm from './components/LoginForm';
 
 function App() {
   return (
-    <Router>
-      <div className="App">
-        <header className="App-header">
-          <h2>Sprint Dad Jokes App</h2>
-        </header>
-        <Route exact path="/" component={LoginForm} />
-      </div>
-    </Router>
+    <div className="App">
+      <header className="App-header">
+        <h2>Sprint Dad Jokes App</h2>
+      </header>
+    </div>
   );
 }
 
